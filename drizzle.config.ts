7c13a8f7bdea48b7955e6321e41c9bb235315f73
lakubuDavid <Lakubudavid@gmail.com>
2025-04-05 01:14:04 +0400

    fix: upt vars in code maybe

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   schema: './src/db/schemas',
   dialect: 'turso',
   dbCredentials: {
-    url: process.env["TURSO_DB_URL"] || "",
-    authToken: process.env["TURSO_TOKEN"] || "",
+    url: process.env["TURSO_DB_URL"] || "libsql://y2p-lakubudavid.turso.io",
+    authToken: process.env["TURSO_TOKEN"] || "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3Mzg5ODg0MzcsImlkIjoiYTA4ZTdhMGMtMjgwZi00YzdhLWFiYWYtZDY5MGU1ODNkMzAwIn0.uB50IPnzRrThsW8qlv9-wzomnf_2StJ5Z9TYdTpzQckcHRgFLRY0rorCvI7uw8BBNnq8-NDNsZ4RFcbRh3gFAQ",
   },
 });
